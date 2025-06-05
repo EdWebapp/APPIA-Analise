93fa56a1c936f69f090a090b8f6d4acf38f0b37d
EdWebapp <canadaiago@gmail.com>
2025-06-05 00:46:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { buscarJogadores, buscarClubes } = require('./services/footballAPI');
+const { buscarJogadores, buscarClubes } = require('./footballAPI');
 
 const app = express();
 const PORT = 3001;
